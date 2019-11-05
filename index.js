7cc833dbde669a8d6232cc5585dcf1f49b1b2f75
github username <github email address>
2019-11-05 15:44:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+ function map(array, callback) {
+  let newArray = [];
+  for (let i=0; i < array.length; i++) {
+    newArray.push(callback(array[i]));
+  }
+  return newArray;
+ }
+
+ 
+function reduce(array, callback, startingPoint) {
+  let start;
+  let i;
+  if (startingPoint) { 
+    start = startingPoint;
+    i = 0;
+  } else {
+    start = array[0];
+    i = 1;
+  }
+  for (i; i < array.length; i++) {
+    start = callback(array[i], start)
+  }
+  return start;
+ }
